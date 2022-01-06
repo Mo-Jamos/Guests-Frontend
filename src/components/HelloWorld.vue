28f107695b5c2c2e734045db7dc8f7243c858008
EmadBaroudi <72658412+EmadBaroudi@users.noreply.github.com>
2022-01-06 16:47:43 +0100

    FrontEnd update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,33 @@
 <template>
+  <img style="margin-left: 100px; width:500px;" alt='Vue logo' src='../assets/schriftzug.png'>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button type="button" class="btn btn-primary">Primary</button>
+    <div
+      style='
+        margin: 0 auto;
+        width:300px;
+        padding: 16px 0 16px 0;
+        border: 1px solid black;
+        background-color: magenta;
+      '
+    >
+      <form
+        action='SENDEADRESSE'
+        id='ft-form'
+        method='POST'
+        accept-charset='UTF-8'
+      >
+        <h1 style="color:white">Testüberschrift</h1>
+        <h6 style="color:white">Unterüberschrift</h6>
+        <input placeholder="Vorname" type='text' name='Name' required /><br><br>
+        <input placeholder="Nachname" type='text' name='E-Mail' required /><br><br>
+        <input placeholder="E-Mail-Adresse" type='email' name='E-Mail' required /><br><br>
+        <input placeholder="Telefon" type="tel" name='Telefon' required /><br><br>
+        <textarea rows='4' placeholder="Geben Sie hier Ihre Nachricht ein..." name='Nachricht' required></textarea>
+        <div class='btns'>
+          <input type='submit' value='Absenden' />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 <script>
